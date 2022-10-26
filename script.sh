aa8fe74ce64104f4c6436b893ad3b900ed3ce7e8
droperox <110151267+droperox@users.noreply.github.com>
2022-10-26 14:35:39 -0300

    Add files via upload

---
+++ script.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+echo "Criando as imagens.................."
+
+docker build -t droperox/projeto-backend:1.0 backend/.
+docker build -t droperox/projeto-database:1.0 database/.
+
+echo "Realizando o push das imagens.................."
+
+docker push droperox/projeto-backend:1.0
+docker push droperox/projeto-database:1.0
+
+echo "Criando serviços no cluster kubernetes.................."
+
+kubectl apply -f ./services.yml
+
+echo "Realizando os deployments.................."
+
+kubectl apply -f ./deployment.yml
